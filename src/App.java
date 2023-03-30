89b91e6306cd0758cd4062cd4577febc031caa14
Júlia Freitas <juliafreitas@alu.ufc.br>
2023-03-29 23:17:19 -0300

    adicionando cores no terminal

--- src/App.java
+++ src/App.java
@@ -14,21 +14,21 @@ public class App {
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse <String> response = client.send(request, BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        
+
         // extrair só os dados que interessam (título, poster, classificação)
         JsonParser parser = new JsonParser();
-        List<Map<String, String>> listaDeFIlmes = parser.parse(body) ;
-        
-        // exibir e manipular os dados 
-        for (Map<String,String> filme : listaDeFIlmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+        List<Map<String, String>> listaDeFIlmes = parser.parse(body);
+
+        // exibir e manipular os dados
+        // exibir os 5 primeiros tops filmes
+        for (int i = 0; i < 5; i++) {
+            Map<String, String> filme = listaDeFIlmes.get(i);
+            System.out.println("\u001b[38;5;208;255;1mTítulo >> \u001b[m\u001b[4m" + filme.get("title") + "\u001b[m");
+            System.out.println("\u001b[38;5;208;255;1mUser rating >> \u001b[m" + filme.get("imDbRating"));
+            System.out.println("\u001b[38;5;208;255mImagem >> \u001b[m" + filme.get("image"));
             System.out.println();
-
-
         }
 
     }
